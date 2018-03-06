e8c096b9bbb37099a3095706ad7b14f6716bf682
Aaron M Green <txhc010888@gmail.com>
2018-03-05 19:20:53 -0600

    Added album component and route to it

---
+++ src/components/Album.js
@@ -0,0 +1,13 @@
+/*This is supposed to be empty....oops
+it causes an error unless I have this, so I did it 
+to make sure it's working :D */
+
+import React from 'react';
+
+const Album = () => (
+  <section className="album">
+    Album page will go here
+  </section>
+);
+
+export default Album;
