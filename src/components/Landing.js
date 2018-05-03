cda3462e6ac7b19fe12479b7a5ed77cba2b80ec1
Aaron M Green <txhc010888@gmail.com>
2018-05-03 06:26:31 -0500

    Added bootstrap link to index.html so the elements worked

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,34 +3,37 @@ import {Grid, Row, Col} from 'react-bootstrap';
 import '.././styles/landing.css';
 
 const Landing = () => (
-  <Grid className="fluid">
-    <Row className="landing show-grid">
-      <Col xs={12} className="selling-points">
+  <Grid>
+    <section className="landing show-grid">
+      <div className="selling-points">
         <h1 id="landing-title">Turn the Music Up!</h1>
 
-        <Col xs={12} md={4}>
-          <Col xs={12} md={4} xsPull={12} className="point one">
+        <Row>
+          <Col xs={12} md={4} className="point one">
             <h3 className="point-title">Choose your music</h3>
-            <Col xs={12} md={4} xsPush={12} className="point-description one">
+            <div className="point-description one">
               <p>The world is full of music; why should you have to listen to music that someone else chose?</p>
-            </Col>
+            </div>
           </Col>
-          <Col xs={12} md={4} xsPull={12} className="point two">
-            <h3 className="point-title">Unlimited, streaming, ad-free</h3>
-            <Col xs={12} md={4} xsPush={12} className="point-description two">
-              <p>No arbitrary limits. No distractions.</p>
-            </Col>
-          </Col>
-          <Col xs={12} md={4} xsPull={12} className="point three">
-            <h3 className="point-title">Mobile enabled</h3>
-            <Col xs={12} md={4} xsPush={12} className="point-description three">
-              <p>Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
-            </Col>
-          </Col>
-        </Col>
 
-      </Col>
-    </Row>
+          <Col xs={12} md={4} className="point two">
+            <h3 className="point-title">Unlimited, streaming, ad-free</h3>
+            <div className="point-description two">
+              <p>No arbitrary limits. No distractions.</p>
+            </div>
+          </Col>
+
+          <Col xs={12} md={4} className="point three">
+            <h3 className="point-title">Mobile enabled</h3>
+            <div className="point-description three">
+              <p>Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+            </div>
+          </Col>
+
+        </Row>
+
+      </div>
+    </section>
   </Grid>
 );
 
